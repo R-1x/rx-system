900c094eb3a0debdc8acd3d5a35ff50df85d0e5f
R-1x <fasollyals2314@gmail.com>
2026-06-11 02:55:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+const { Client, GatewayIntentBits, EmbedBuilder } = require("discord.js");
+require("dotenv").config();
 
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+});
+
+client.once("ready", () => {
+    console.log(`RX SYSTEM Online: ${client.user.tag}`);
+});
+
+// أمر تجريبي
+client.on("messageCreate", (message) => {
+    if (message.author.bot) return;
+
+    if (message.content === "!help") {
+        const embed = new EmbedBuilder()
+            .setTitle("RX SYSTEM")
+            .setDescription("البوت شغال 👍")
+            .setColor("Blue");
+
+        message.reply({ embeds: [embed] });
+    }
+});
+
+client.login(process.env.TOKEN);
